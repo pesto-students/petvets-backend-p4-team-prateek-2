c15d28f83b1a43c702bf8cdb560e164550742039
Swatijl <swatisam76@gmail.com>
2023-01-10 22:31:29 +0530

    added vetDetail in appointment

--- routes/api/appointmentHistory.js
+++ routes/api/appointmentHistory.js
@@ -2,37 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 const Appointment = require('../../models/appointments.model');
+const User = require('../../models/users.model');
 
 router.get('/:userId', async (req, res) => {
   const userId = req.params.userId;
-  try {
-    const appointment = await Appointment.find({
-      userId: userId,
-      // paymentStatus: 'success'
-    }).lean()
-    //   .aggregate([
-    //     {
-    //       $lookup: {
-    //         from: 'user',
-    //         localField: 'vetDetail',
-    //         foreignField: 'userId',
-    //         as: 'myCustomResut',
-    //       },
-    //     },
-    //   ])
-    //   .exec((err, result) => {
-    //     console.log("res", result)
-    //     if (err) {
-    //       console.log('error', err);
-    //     }
-    //     if (result) {
-    //       console.log(result);
-    //     }
-    //   });
-    res.status(200).json(appointment);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
+  return Appointment.find({
+    userId: userId,
+    // paymentStatus: 'success'
+  })
+    .then((result) => {
+      result.map((app) => {
+        return User.find({
+          userId: app.vetId,
+        }).then((user) => {
+          app['vetDetail'] = user[0];
+          res.status(200).json(result);
+        });
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error });
+    });
 });
 
 module.exports = router;

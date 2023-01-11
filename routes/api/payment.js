577a2f0f45e379b2c0e7a764eb4ed2b1e705d626
abhishekraje30 <abhishekraje30@gmail.com>
2023-01-11 23:52:04 +0530

    clinic details added in user schema

--- routes/api/payment.js
+++ routes/api/payment.js
@@ -1,42 +1,42 @@
-const express = require("express");
-const path = require("path");
-const Razorpay = require("razorpay");
-const shortid = require("shortid");
-const bodyParser = require("body-parser");
-const crypto = require("crypto");
-const cors = require("cors");
+const express = require('express');
+const path = require('path');
+const Razorpay = require('razorpay');
+const shortid = require('shortid');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const cors = require('cors');
 const router = express.Router();
 
-require("dotenv").config();
+require('dotenv').config();
 
 let razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-router.post("/verification", (req, res) => {
-  const secret = "razorpaysecret";
+router.post('/verification', (req, res) => {
+  const secret = 'razorpaysecret';
 
-  const shasum = crypto.createHmac("sha256", secret);
+  const shasum = crypto.createHmac('sha256', secret);
   shasum.update(JSON.stringify(req.body));
-  const digest = shasum.digest("hex");
+  const digest = shasum.digest('hex');
 
-  console.log(digest, req.headers["x-razorpay-signature"]);
+  console.log(digest, req.headers['x-razorpay-signature']);
 
-  if (digest === req.headers["x-razorpay-signature"]) {
-    console.log("request is legit");
+  if (digest === req.headers['x-razorpay-signature']) {
+    console.log('request is legit');
     res.status(200).json({
-      message: "OK",
+      message: 'OK',
     });
   } else {
-    res.status(403).json({ message: "Invalid" });
+    res.status(403).json({ message: 'Invalid' });
   }
 });
 
-router.post("/razorpay", async (req, res) => {
+router.post('/razorpay', async (req, res) => {
   const payment_capture = 1;
   const amount = 500;
-  const currency = "INR";
+  const currency = 'INR';
 
   const options = {
     amount,
